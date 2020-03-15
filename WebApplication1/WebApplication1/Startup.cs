df11f8f1f44c58b9cfce4e71d1b39b8cca33bd9b
tuan anh <anh.nt.1196@gmail.com>
2020-03-15 20:51:19 +0700

    ok

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -46,6 +46,9 @@ namespace WebApplication1
             app.UseMvc();
 
             // add test comment 1 
+            // add test commetn2
+            // add test comment 3
+            // add test comment 4
         }
     }
 }
